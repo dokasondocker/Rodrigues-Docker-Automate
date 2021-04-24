c7efb9d84b450b993073e7c620c0869a8079f9d4
melchilio <65927599+melchilio@users.noreply.github.com>
2021-04-24 17:16:14 -0600

    Update dockerimages4.sh

--- dockerimages4.sh
+++ dockerimages4.sh
@@ -8,6 +8,8 @@ IND=index.html
 c=centos
 U=ubuntu
 H=httpd
+echo "Welcome, please enter your name"
+read n
 echo "Please select an image"
 read i
 echo "Please select the version/tags"
@@ -15,16 +17,16 @@ read v
 if [ ${i} = centos ];
 then
 echo "<h1> This is my script to build docker images" > $IND
-echo "FROM centos:$v" >> $D
-echo "MAINTAINER Rodrigue" >> $D
+echo "FROM ${c}:$v" >> $D
+echo "MAINTAINER ${n}" >> $D
 echo "EXPOSE 80" >> $D
 echo "COPY ./index.html /var/www/html" >> $D
 echo "CMD apachectl -D FOREGROUND" >> $D
 docker build -t $i:$v .
 elif [ ${i} = ubuntu ];
 then
-echo "FROM ubuntu:$v" >> $D
-echo "MAINTAINER Rodrigue" >> $D
+echo "FROM ${U}:$v" >> $D
+echo "MAINTAINER ${n}" >> $D
 echo "apt-get update -y"
 echo "EXPOSE 80" >> $D
 echo "COPY ./index.html /var/www/html" >> $D
@@ -32,8 +34,8 @@ echo "CMD apachectl -D FOREGROUND" >> $D
 docker build -t $U:$v .
 elif [ ${i} = httpd ];
 then
-echo "FROM httpd:$v" >> $D
-echo "MAINTAINER Rodrigue" >> $D
+echo "FROM ${H}:$v" >> $D
+echo "MAINTAINER ${n}" >> $D
 echo "RUN apt-get update -y" >> $D
 echo "COPY ./index.html /usr/local/apache2/htdocs/" >> $D
 docker build -t $H:$v .
@@ -46,6 +48,6 @@ then
 rm -f $D
 fi
 sleep 2
-echo "Thank you for your business"
+echo "Enjoy your images"
 sleep 2
 echo "Enjoy your image"
